54c571932985c4a7286e3babcd46d37a4aa18b9f
mingshu230 <48084225+mingshu230@users.noreply.github.com>
2022-08-13 13:30:59 +0900

    oh yes

---
+++ p5/empty-example/portrait.js
@@ -0,0 +1,15 @@
+function setup() {
+  createCanvas(500, 500);
+}
+
+function draw() {
+  background(500);
+  line(150,0,300,300);
+
+
+  rectMode(center);
+  rect(200,200,100,100);
+  ellipse(250,250,30,30);
+
+
+}
---
+++ p5/empty-example/sketch.js
@@ -0,0 +1,33 @@
+function setup() {
+  createCanvas(500, 500);
+}
+
+function draw() {
+  background(500);
+  strokeWeight(2);
+  stroke(51);
+
+  line(150,50,300,300);
+ //   fill(255,0,0);    
+  line(170,50,320,300);
+  line(190,50,340,300);
+  line(210,50,360,300);
+  line(230,50,380,300);
+  line(250,50,400,300);
+  line(270,50,420,300);
+  line(290,50,440,300);
+  line(310,50,460,300);
+
+  
+  ellipse(100,200,40,30);
+  ellipse(100,200,20,20);
+  fill
+
+
+  line(100,350,70,340);
+
+
+
+
+
+}
